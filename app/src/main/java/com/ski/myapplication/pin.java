80cf277e9a50a64a17bc085eeb7af0f70d49bbbe
Viriya Lucky <viriyaluckyoo@gmail.com>
2020-06-24 14:59:36 +0700

    first commit

---
+++ app/src/main/java/com/ski/myapplication/pin.java
@@ -0,0 +1,55 @@
+package com.ski.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.chaos.view.PinView;
+
+public class pin extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin);
+
+        final PinView pinView = findViewById(R.id.firstPinView);
+        pinView.setTextColor(Color.BLACK);
+
+        pinView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                int a = 1;
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                int a = 1;
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        pinView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        pinView.setLineColor(Color.BLACK);
+        pinView.setItemBackgroundColor(Color.WHITE);
+        pinView.setHideLineWhenFilled(false);
+    }
+
+
+}
